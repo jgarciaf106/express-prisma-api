1ce97cd4d016527fb19b76c1100d073b3d1be527
Andres Garcia <joseandresgarciafernandez@dev.azure.com>
2022-03-21 10:45:19 -0600

    added scripts

--- docs/welcome.js
+++ docs/welcome.js
@@ -3,9 +3,9 @@ console.log(`
 \x1b[34m Install dependencies:
     \x1b[33m $ npm install \x1b[0m to install dependencies.
 \x1b[34m Seed the database:
-    \x1b[33m $ npx prisma migrate dev --name init \x1b[0m to seed the database.
+    \x1b[33m $ npm run config \x1b[0m to seed the database.
 \x1b[34m You can run the 💻 server by typing: 
     \x1b[33m $ npm run start \x1b[0m to start the server.
 \x1b[34m You can start Prisma Studio by typing: 
-    \x1b[33m $ npx prisma studio \x1b[0m to start Prisma Studio.
+    \x1b[33m $ npm run studio \x1b[0m to start Prisma Studio.
 `)
