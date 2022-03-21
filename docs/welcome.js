067267be432cf20256d5e0f454f76e72e60c3ba6
Andres Garcia <joseandresgarciafernandez@dev.azure.com>
2022-03-21 09:49:58 -0600

    Updated Config

--- docs/welcome.js
+++ docs/welcome.js
@@ -1,9 +1,11 @@
 console.log(`
 😃 Welcome to the Express/Prisma Boilerplate 🎊!
-\x1b[34m Seeed the database:
-    \x1b[33m $ npx prisma migrate dev --name init \x1b[0m to seed the database
+\x1b[34m Install dependencies:
+    \x1b[33m $ npm install \x1b[0m to install dependencies.
+\x1b[34m Seed the database:
+    \x1b[33m $ npx prisma migrate dev --name init \x1b[0m to seed the database.
 \x1b[34m You can run the 💻 server by typing: 
-    \x1b[33m $ npm run start \x1b[0m to start the server
+    \x1b[33m $ npm run start \x1b[0m to start the server.
 \x1b[34m You can start Prisma Studio by typing: 
-    \x1b[33m $ npx prisma studio \x1b[0m to start Prisma Studio
+    \x1b[33m $ npx prisma studio \x1b[0m to start Prisma Studio.
 `)
